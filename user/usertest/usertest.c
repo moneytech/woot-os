25f58831181a1d2dc64d8307bcfd9f1347a43912
pvc <pvc.meheha@gmail.com>
2019-02-24 14:37:34 +0100

    tested environment variables

--- user/usertest/usertest.c
+++ user/usertest/usertest.c
@@ -4,12 +4,18 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+extern char **environ;
+
 int main(int argc, char *argv[])
 {
     printf("Hello from userland!\n");
     printf("pid: %d\n", getpid());
     printf("tid: %d\n", (int)syscall(SYS_gettid));
     printf("cwd: %s\n", getcwd(NULL, 0));
+    putenv("TROLO=lolo");
+    char *env = environ[0];
+    for(int i = 0; env; env = environ[++i])
+        printf("%s\n", env);
     printf("Bye from userland!\n");
     return 0;
 }
